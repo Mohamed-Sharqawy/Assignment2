2239d2f2ff60da28558b53557b8551a6df943607
Mohamed-Sharqawy <msharqawy21@gmail.com>
2024-10-25 22:27:15 +0300

    Add files via upload
    This is the second assignment :
    this assignment contains two files first the file calc.py that contains four functions that perform basic mathematical operations add, sub, divide and multiply by taking two numbers as arguments.
    the second file is prompting the user to enter two numbers and choose whether to perform any of the operations or just close the app

---
+++ calc.py
@@ -0,0 +1,14 @@
+def add(num1, num2):
+    print(num1 + num2)
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+def divide(num1, num2):
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+def subtract(num1, num2):
+    print(num1 - num2)
